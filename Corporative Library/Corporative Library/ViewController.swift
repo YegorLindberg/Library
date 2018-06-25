5a0e81984c235a99d180cee583e0818d544706ba
YegorLindberg <mr.tsarap@mail.ru>
2018-06-25 18:10:10 +0300

    добавил первый вариант структуры и немного поигрался с main.storyboard

--- Corporative Library/Corporative Library/ViewController.swift
+++ Corporative Library/Corporative Library/ViewController.swift
@@ -8,17 +8,52 @@
 
 import UIKit
 
+struct Author: Decodable {
+    var name: String
+    var surname: String
+}
+
+struct someBook: Decodable {
+    var name: String
+    var authors: [Author]
+    var available: String
+    var year: String
+    var description: String
+    var link: String
+}
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let urlString = "http://private-0fc390-corporative0library.apiary-mock.com/simplebook.json"
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            guard let data = data else { return }
+            guard error == nil else { return }
+            do {
+                let someNewBook = try JSONDecoder().decode(someBook.self, from: data)
+                print(someNewBook, "\n")
+                print(someNewBook.link as Any, "\n")
+                print(someNewBook.authors[0].name, "\n")
+                print("\n")
+            } catch let error {
+                print(error)
+            }
+            
+        }.resume()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
